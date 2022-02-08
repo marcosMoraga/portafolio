79c03d4e4114b7588832050ca920d0d10eca0993
19037908 <moraga.hermosilla@hotmail.com>
2022-02-07 23:27:13 -0300

    responsive proyect-3

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useMemo } from "react";
+import React, { useState, useRef, useMemo, useEffect } from "react";
 import Link from "next/link";
 import NavRolexItem from "../../components/NavRolexItem";
 import Head from "next/head";
@@ -7,117 +7,144 @@ import { BsChevronRight, BsChevronLeft } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
 import { HiOutlineHome } from "react-icons/hi";
 import Image from 'next/image'
-const proyects = [
-    {
-        name: "mytinerary",
-        url: "https://mytinerary-moraga.netlify.app/",
-        description: {
-            role: "Este proyecto se caracteriza por su desarrollo individual (full stack), atendiendo al patrón de diseño MVC.",
-            dificulties:
-                "En este proyecto lo mas complejo se evidencio a la hora de utilizar estados en los que se necesitaban en mas de un componente, inicialmente teniendo que transportar la información mediante las propiedades de otros componentes.",
-            solution:
-                "Una de las soluciones mas acertadas, fue el uso de Redux. La que fue elegida por sobre otras (context hook), ya que se prentende que el actual proyecto escale a una gran madurez",
-            tech: [
-                "React",
-                "Redux",
-                "NodeJs",
-                "ExpressJs",
-                "Mongodb",
-                "JOI",
-                "Passport",
-                "JWT",
-                "Mongoose",
-                "Google OAuth",
-                "Git",
-                "Postman",
-                "MongoDBCompass",
-            ],
-            features: [
-                "Login",
-                "Registro de usuarios",
-                "Likes",
-                "Comentarios",
-                "Validador - formulario de registro",
-            ],
-        },
-        img: "/proyect-3.jpg",
-    },
-    {
-        name: "broke & fix",
-        description: {
-            role: "En broke & fix tome el rol de product owner y fui lider en el equipo backend, logrando un producto minimo viable",
-            dificulties:
-                "En un comienzo se vio afectado por la poca o nula comunicación entre los equipos de desarrollos (backend - frontend)",
-            solution:
-                "Se establece seguir al pie de la letra la metodologia scrum, donde diariamente en no mas de 20 min, se mencionaba el estado en que se encontraba cada equipo",
-            tech: [
-                "React",
-                "Redux",
-                "NodeJs",
-                "ExpressJs",
-                "Mongodb",
-                "JOI",
-                "Passport",
-                "JWT",
-                "Mongoose",
-                "Google OAuth",
-                "Git",
-                "Postman",
-                "MongoDBCompass",
-            ],
-            features: [
-                "Login",
-                "Registro de usuarios",
-                "Rating",
-                "Comentarios",
-                "Validador - formulario de registro",
-                "Panel de administración",
-            ],
-        },
-        url: "https://brokandfix.herokuapp.com/",
-        img: "/proyect-1.jpg",
-    },
-    {
-        name: "proyecto z",
-        description: {
-            role: "En Proyecto Z, buscamos familiarizarnos con el mundo de los NFT, siendo mi participacion clave a la hora de desarrollar funcionalidades (tanto de backend como de frontend) propios del sistema en cuestión",
-            dificulties:
-                "AL ser un proyecto relacionado con un rubro nuevo y con un tiempo muy limitado, existian grandes dificultades a la hora de abordar funcionalidades que derivan del sistema en desarrollo",
-            solution:
-                "La solución mas proxima fue esclarecer con el equipo de desarrollo el alcance del proyecto, logrando simular un marketplace de ofertas y demandas",
-            tech: [
-                "React",
-                "Redux",
-                "NodeJs",
-                "ExpressJs",
-                "Mongodb",
-                "Websockets",
-                "JOI",
-                "Passport",
-                "JWT",
-                "Mongoose",
-                "Google OAuth",
-                "Git",
-                "Postman",
-                "MongoDBCompass",
-                "Paypal",
-            ],
-            features: [
-                "Compra/venta de NFT",
-                "Paypal",
-                "Juego",
-                "Suscripción",
-                "Dashboard",
-                "Graficos en tiempo real",
-            ],
-        },
-        url: "https://proyectzzzz.netlify.app/",
-        img: "/proyect-2.jpg",
-    },
-];
+import useMediaQuery from '@mui/material/useMediaQuery';
 
+const proyects = [{
+    name: "mytinerary",
+    url: "https://mytinerary-moraga.netlify.app/",
+    description: {
+        role: "Este proyecto se caracteriza por su desarrollo individual (full stack), atendiendo al patrón de diseño MVC.",
+        dificulties:
+            "En este proyecto lo mas complejo se evidencio a la hora de utilizar estados en los que se necesitaban en mas de un componente, inicialmente teniendo que transportar la información mediante las propiedades de otros componentes.",
+        solution:
+            "Una de las soluciones mas acertadas, fue el uso de Redux. La que fue elegida por sobre otras (context hook), ya que se prentende que el actual proyecto escale a una gran madurez",
+        tech: [
+            "React",
+            "Redux",
+            "NodeJs",
+            "ExpressJs",
+            "Mongodb",
+            "JOI",
+            "Passport",
+            "JWT",
+            "Mongoose",
+            "Google OAuth",
+            "Git",
+            "Postman",
+            "MongoDBCompass",
+        ],
+        features: [
+            "Login",
+            "Registro de usuarios",
+            "Likes",
+            "Comentarios",
+            "Validador - formulario de registro",
+        ],
+    },
+    img: '/proyect-3.jpg'
+},
+{
+    name: "broke & fix",
+    description: {
+        role: "En broke & fix tome el rol de product owner y fui lider en el equipo backend, logrando un producto minimo viable",
+        dificulties:
+            "En un comienzo se vio afectado por la poca o nula comunicación entre los equipos de desarrollos (backend - frontend)",
+        solution:
+            "Se establece seguir al pie de la letra la metodologia scrum, donde diariamente en no mas de 20 min, se mencionaba el estado en que se encontraba cada equipo",
+        tech: [
+            "React",
+            "Redux",
+            "NodeJs",
+            "ExpressJs",
+            "Mongodb",
+            "JOI",
+            "Passport",
+            "JWT",
+            "Mongoose",
+            "Google OAuth",
+            "Git",
+            "Postman",
+            "MongoDBCompass",
+        ],
+        features: [
+            "Login",
+            "Registro de usuarios",
+            "Rating",
+            "Comentarios",
+            "Validador - formulario de registro",
+            "Panel de administración",
+        ],
+    },
+    url: "https://brokandfix.herokuapp.com/",
+    img: "/proyect-1.jpg",
+},
+{
+    name: "proyecto z",
+    description: {
+        role: "En Proyecto Z, buscamos familiarizarnos con el mundo de los NFT, siendo mi participacion clave a la hora de desarrollar funcionalidades (tanto de backend como de frontend) propios del sistema en cuestión",
+        dificulties:
+            "AL ser un proyecto relacionado con un rubro nuevo y con un tiempo muy limitado, existian grandes dificultades a la hora de abordar funcionalidades que derivan del sistema en desarrollo",
+        solution:
+            "La solución mas proxima fue esclarecer con el equipo de desarrollo el alcance del proyecto, logrando simular un marketplace de ofertas y demandas",
+        tech: [
+            "React",
+            "Redux",
+            "NodeJs",
+            "ExpressJs",
+            "Mongodb",
+            "Websockets",
+            "JOI",
+            "Passport",
+            "JWT",
+            "Mongoose",
+            "Google OAuth",
+            "Git",
+            "Postman",
+            "MongoDBCompass",
+            "Paypal",
+        ],
+        features: [
+            "Compra/venta de NFT",
+            "Paypal",
+            "Juego",
+            "Suscripción",
+            "Dashboard",
+            "Graficos en tiempo real",
+        ],
+    },
+    url: "https://proyectzzzz.netlify.app/",
+    img: "/proyect-2.jpg",
+},]
 const Modules = () => {
-    const [img, setImg] = useState('/proyect-3.jpg');
+
+    const largeSize = useMediaQuery('(min-width:801px) and (max-width:1023px)');
+    const mediumSize = useMediaQuery('(min-width:501px) and (max-width:800px)');
+    const smallSize = useMediaQuery('(min-width:320px) and (max-width:500px)');
+    const [image, setImg] = useState('/proyect-3.jpg')
+    const [filteredView, setView] = useState({ img: '/proyect-3.jpg' })
+
+
+    const getView = (paramView) => {
+        const filteredView = proyects.find((proyect) => proyect.name === paramView);
+        setView(filteredView);
+
+        demo.current.href = filteredView.url;
+        setDescription({
+            role: filteredView.description.role,
+            dificulties: filteredView.description.dificulties,
+            solution: filteredView.description.solution,
+            tech: filteredView.description.tech,
+            features: filteredView.description.features,
+        });
+    };
+
+    useEffect(() => {
+        const urlImage = filteredView.img.substring(0, 10)
+        const img = largeSize ? `${urlImage}-l.jpg` : mediumSize ? `${urlImage}-m.jpg` : smallSize ? `${urlImage}-sm.jpg` : `${urlImage}.jpg`;
+        setImg(img)
+    }, [largeSize, mediumSize, smallSize, getView]);
+
+
     const demo = useRef();
     const [step, setStep] = useState(1);
     const [description, setDescription] = useState({
@@ -128,19 +155,6 @@ const Modules = () => {
         features: proyects[0].description.features,
     });
     const [hover, setHover] = useState(false);
-
-    const getView = (paramView) => {
-        const filteredView = proyects.find((proyect) => proyect.name === paramView);
-        setImg(filteredView.img);
-        demo.current.href = filteredView.url;
-        setDescription({
-            role: filteredView.description.role,
-            dificulties: filteredView.description.dificulties,
-            solution: filteredView.description.solution,
-            tech: filteredView.description.tech,
-            features: filteredView.description.features,
-        });
-    };
     const renderComponent = useMemo(
         () => (
             <NavRolexItem
@@ -186,7 +200,7 @@ const Modules = () => {
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
 
-                <Image src={img} layout='fill'></Image>
+                <Image src={image} layout='fill'></Image>
                 <article
                     className={styles.article}
                 >
